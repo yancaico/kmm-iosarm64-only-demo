f766459a7f64befc72a457739bab85ba94632244
yuantong <zosidev@163.com>
2023-01-31 20:38:08 +0800

    成功编译了 IOTCamera.framework

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,24 +6,22 @@ plugins {
 
 kotlin {
     android()
-    iosX64()
+//    iosX64()
     iosArm64 {
         compilations.getByName("main") {
             val IOTCamera by cinterops.creating {
                 // Path to .def file
                 defFile("src/nativeInterop/cinterop/IOTCamera.def")
-                compilerOpts("-framework", "IOTCamera", "-F${rootProject
-                    .projectDir}/shared/frameworks/")
+                compilerOpts("-framework", "IOTCamera", "-F${rootProject.projectDir}/shared/frameworks/")
             }
         }
 
         binaries.all {
             // Tell the linker where the framework is located.
-            linkerOpts("-framework", "IOTCamera", "-F${rootProject
-                .projectDir}/shared/frameworks/")
+            linkerOpts("-framework", "IOTCamera", "-F${rootProject.projectDir}/shared/frameworks/")
         }
     }
-    iosSimulatorArm64()
+//    iosSimulatorArm64()
 
     cocoapods {
         summary = "Some description for the Shared Module"
@@ -34,6 +32,7 @@ kotlin {
         framework {
             baseName = "shared"
         }
+        useLibraries()
     }
     
     sourceSets {
@@ -45,23 +44,23 @@ kotlin {
         }
         val androidMain by getting
         val androidTest by getting
-        val iosX64Main by getting
+//        val iosX64Main by getting
         val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
+//        val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
+//            iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+//            iosSimulatorArm64Main.dependsOn(this)
         }
-        val iosX64Test by getting
+//        val iosX64Test by getting
         val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
+//        val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
+//            iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
+//            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
